16b89a47abe2c65c501b8776af69fabd26f17962
Thabish <kadertabish2@gmail.com>
2023-02-04 10:55:52 +0400

    sphere
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ import * as THREE from "three";
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.SphereGeometry(3);
+const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
 	color: "#00ff83",
 });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
+// light
+const light = new THREE.PointLight(0xffffff, 1, 100);
+light.position.set(0, 10, 10);
+scene.add(light);
 // camera
-const camera = new THREE.PerspectiveCamera(45, 800, 600);
+const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+camera.position.z = 20;
 scene.add(camera);
 
 // rendere
