d276cb34c97fa7e53d024489c286ab1a10659833
Thabish <kadertabish2@gmail.com>
2023-02-04 14:42:21 +0400

    cylinder-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- three-react/src/App.jsx
+++ three-react/src/App.jsx
@@ -11,10 +11,25 @@ function App() {
 		test.initalize();
 		test.animate();
 
-		const boxGeometry = new THREE.BoxGeometry(16, 16, 16);
+		const boxGeometry = new THREE.BoxGeometry(1, 1, 1, 1, 16);
 		const boxMaterial = new THREE.MeshNormalMaterial();
 		const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+		boxMesh.position.x = -1;
 		test.scene.add(boxMesh);
+
+		const cylinderGeometry = new THREE.CylinderGeometry(
+			0.5,
+			0.5,
+			1,
+			32,
+			16
+		);
+		const cylinderMaterial = new THREE.MeshNormalMaterial({
+			wireframe: true,
+		});
+		const cylinderMesh = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
+		cylinderMesh.position.x = 1;
+		test.scene.add(cylinderMesh);
 	}, []);
 
 	return (
