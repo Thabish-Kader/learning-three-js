7848c3825dc193454f7f81c75c5f98431afa8a77
Thabish <kadertabish2@gmail.com>
2023-02-04 11:23:13 +0400

    styling
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 import * as THREE from "three";
-
+import "./style.css";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 const scene = new THREE.Scene();
+
+const sizes = {
+	width: window.innerWidth,
+	hieght: window.innerHeight,
+};
 
 const geometry = new THREE.SphereGeometry(3, 64, 64);
 const material = new THREE.MeshStandardMaterial({
@@ -14,12 +20,41 @@ const light = new THREE.PointLight(0xffffff, 1, 100);
 light.position.set(0, 10, 10);
 scene.add(light);
 // camera
-const camera = new THREE.PerspectiveCamera(45, 800 / 600, 0.1, 100);
+const camera = new THREE.PerspectiveCamera(
+	45,
+	sizes.width / sizes.hieght,
+	0.1,
+	100
+);
 camera.position.z = 20;
 scene.add(camera);
 
 // rendere
 const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
-renderer.setSize(800, 600);
+renderer.setSize(sizes.width, sizes.hieght);
+renderer.setPixelRatio(2);
 renderer.render(scene, camera);
+
+// Resize
+window.addEventListener("resize", () => {
+	(sizes.width = window.innerWidth), (sizes.hieght = window.innerHeight);
+	//update camera
+	camera.aspect = sizes.width / sizes.hieght;
+	camera.updateProjectionMatrix();
+	renderer.setSize(sizes.width, sizes.hieght);
+});
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 5;
+
+const loop = () => {
+	controls.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(loop);
+};
+loop();
