fec82b8cc2ed98cb1594c8399a6f090e8c23634b
Thabish <kadertabish2@gmail.com>
2023-02-04 19:06:24 +0400

    react-spring
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- react-fiber/src/App.jsx
+++ react-fiber/src/App.jsx
@@ -1,36 +1,46 @@
 import { useState, useRef } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
+import { Canvas, extend, useFrame, useThree } from "@react-three/fiber";
+// import { Orbitcontrols } from "three/examples/jsm/controls/OrbitControls";
 import { useSpring, a } from "@react-spring/three";
+// extend({ Orbitcontrols });
+
+// const Controls = () => {
+// 	const { camera, gl } = useThree();
+// 	const orbitRef = useRef();
+// 	useFrame(() => {
+// 		orbitRef.current.update();
+// 	});
+// 	return <orbitControls args={[camera, gl.domElement]} ref={orbitRef} />;
+// };
+
+const Box = () => {
+	const [hoverd, setHoverd] = useState(false);
+	const [active, setActive] = useState(false);
+
+	const props = useSpring({
+		scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+		color: hoverd ? "hotpink" : "grey",
+	});
+
+	return (
+		<a.mesh
+			onPointerOver={() => setHoverd(true)}
+			onPointerOut={() => setHoverd(false)}
+			onClick={() => setActive(!active)}
+			scale={props.scale}
+		>
+			{/* <orbitcontrols /> */}
+			<boxGeometry attach="geometry" args={[1, 1, 1]} />
+			<a.meshBasicMaterial attach="material" color={props.color} />
+		</a.mesh>
+	);
+};
+
 function App() {
-	const Box = () => {
-		const [hoverd, setHoverd] = useState(false);
-		const [active, setActive] = useState(false);
-		const props = useSpring({
-			scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
-			color: hoverd ? "hotpink" : "grey",
-		});
-		const meshRef = useRef(null);
-
-		useFrame(() => {
-			meshRef.current.rotation.y += 0.01;
-		});
-		return (
-			<a.mesh
-				ref={meshRef}
-				onPointerOver={() => setHoverd(true)}
-				onPointerOut={() => setHoverd(false)}
-				onClick={() => setActive(!active)}
-				scale={props.scale}
-			>
-				<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-				<a.meshBasicMaterial attach="material" color={props.color} />
-			</a.mesh>
-		);
-	};
-
 	return (
 		<div className="App">
 			<Canvas>
+				{/* <Controls /> */}
 				<Box />
 			</Canvas>
 		</div>
