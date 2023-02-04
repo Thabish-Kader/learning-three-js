daa7337abac227a75f0efd36dab7a90718019b85
Thabish <kadertabish2@gmail.com>
2023-02-04 16:47:02 +0400

    react-fiber
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- react-fiber/src/App.jsx
+++ react-fiber/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
+import { Canvas } from "react-three-fiber";
+import { useSpring, a } from "@react-spring/three";
 function App() {
-	const [count, setCount] = useState(0);
+	const Box = () => {
+		const [hoverd, setHoverd] = useState(false);
+		const [active, setActive] = useState(false);
+		const props = useSpring({
+			scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+			color: hoverd ? "hotpink" : "grey",
+		});
+		return (
+			<a.mesh
+				onPointerOver={() => setHoverd(true)}
+				onPointerOut={() => setHoverd(false)}
+				onClick={() => setActive(!active)}
+				scale={props.scale}
+			>
+				<boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+				<a.meshBasicMaterial attach="material" color={props.color} />
+			</a.mesh>
+		);
+	};
 
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Canvas>
+				<Box />
+			</Canvas>
+		</div>
+	);
 }
 
 export default App;
