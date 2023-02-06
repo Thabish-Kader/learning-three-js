c0c69739a3d13e25200ba82a1cd1495557f889ce
Thabish <kadertabish2@gmail.com>
2023-02-06 12:01:59 +0400

    geomerty
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- camera/src/script.js
+++ camera/src/script.js
@@ -1,87 +1,93 @@
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
 
-// Sizes
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Object
+ */
+// const geometry = new THREE.BufferGeometry();
+// const count = 50;
+// const positionsArray = new Float32Array(count * 3 * 3);
+// for (let i = 0; i < count * 3 * 3; i++) {
+// 	positionsArray[i] = (Math.random() - 0.5) * 4;
+// }
+// const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
+// geometry.setAttribute("position", positionsAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+	color: 0xff0000,
+});
+
+const mesh = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), material);
+scene.add(mesh);
+
+/**
+ * Sizes
+ */
 const sizes = {
 	width: window.innerWidth,
 	height: window.innerHeight,
 };
 
 window.addEventListener("resize", () => {
+	// Update sizes
 	sizes.width = window.innerWidth;
 	sizes.height = window.innerHeight;
 
+	// Update camera
 	camera.aspect = sizes.width / sizes.height;
 	camera.updateProjectionMatrix();
 
+	// Update renderer
 	renderer.setSize(sizes.width, sizes.height);
 	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
-window.addEventListener("dblclick", () => {
-	if (!document.fullscreenElement) {
-		canvas.requestFullscreen();
-	} else {
-		document.exitFullscreen();
-	}
-});
-
-// Scene
-const scene = new THREE.Scene();
-
-// Object
-const mesh = new THREE.Mesh(
-	new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-	new THREE.MeshBasicMaterial({ color: 0xff0000 })
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+	75,
+	sizes.width / sizes.height,
+	0.1,
+	100
 );
-scene.add(mesh);
-
-// cursor
-const cursor = {
-	x: 0,
-	y: 0,
-};
-window.addEventListener("mousemove", (event) => {
-	cursor.x = event.clientX / sizes.width - 0.5;
-	cursor.y = -(event.clientY / sizes.height - 0.5);
-});
-
-// Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-// const aspectRatio = sizes.width / sizes.height;
-// const camera = new THREE.OrthographicCamera(
-// 	-1 * aspectRatio,
-// 	1 * aspectRatio,
-// 	1,
-// 	-1,
-// 	0.1,
-// 	100
-// );
-// camera.position.x = 2;
-// camera.position.y = 2;
 camera.position.z = 3;
-camera.lookAt(mesh.position);
 scene.add(camera);
 
-// controls
+// Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
 	canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock();
 
 const tick = () => {
 	const elapsedTime = clock.getElapsedTime();
 
+	// Update controls
 	controls.update();
+
 	// Render
 	renderer.render(scene, camera);
 
