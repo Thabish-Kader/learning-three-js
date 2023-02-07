609f57f7775f1b1f070f386377baf336c4abd131
Thabish <kadertabish2@gmail.com>
2023-02-07 10:14:56 +0400

    changes-folder
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- camera/src/script.js
+++ camera/src/script.js
@@ -2,6 +2,24 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import GUI from "lil-gui";
 import gsap from "gsap";
+
+const gui = new GUI();
+
+// const textureLoader = new THREE.TextureLoader();
+// const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
+// const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+// const doorAmbientOcclusionTexture = textureLoader.load(
+// 	"/textures/door/ambientOcclusion.jpg"
+// );
+// const doorHeightTexture = textureLoader.load("/textures/door/height.jpg");
+// const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
+// const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+// const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+// const matcapTexture = textureLoader.load("/textures/door/metalness.jpg");
+// const gradientTexture = textureLoader.load("/textures/gradients/5.jpg");
+
+// debug
+
 /**
  * Base
  */
@@ -11,11 +29,32 @@ const canvas = document.querySelector("canvas.webgl");
 // Scene
 const scene = new THREE.Scene();
 
+// texture
+
 // Object
-const material = new THREE.MeshBasicMaterial({ color: "red" });
+// const material = new THREE.MeshBasicMaterial();
+// material.map = doorColorTexture;
+// // material.color = new THREE.Color("red");
+// // material.wireframe = true;
+// // material.opacity = 0.5;
+// material.transparent = true;
+// material.alphaMap = doorAlphaTexture;
+// material.side = THREE.DoubleSide;
+// light
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 0.5);
+scene.add(pointLight);
+const material = new THREE.MeshStandardMaterial();
+
 const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 16, 16), material);
 
 const plane = new THREE.Mesh(new THREE.PlaneGeometry(1, 1), material);
+material.side = THREE.DoubleSide;
+
+plane.geometry.setAttribute("uv2", new THREE.BufferAttribute());
+
 sphere.position.x = -1.5;
 const torus = new THREE.Mesh(
 	new THREE.TorusGeometry(0.3, 0.2, 16, 32),
@@ -24,6 +63,8 @@ const torus = new THREE.Mesh(
 torus.position.x = 1.5;
 scene.add(sphere, plane, torus);
 
+gui.add(material, "metalness", 0, 1, 0.001);
+gui.add(material, "roughness", 0, 1, 0.001);
 /**
  * Sizes
  */
@@ -86,9 +127,7 @@ const tick = () => {
 	plane.rotation.y = 0.1 * elapsedTime;
 
 	sphere.rotation.x = 0.1 * elapsedTime;
-
 	torus.rotation.x = 0.1 * elapsedTime;
-
 	plane.rotation.x = 0.1 * elapsedTime;
 
 	// Render
