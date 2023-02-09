761af07829a0cbb47993fa5733f2ddcbfe6f9621
Thabish <kadertabish2@gmail.com>
2023-02-09 14:09:26 +0400

    node
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- scroll/src/script.js
+++ scroll/src/script.js
@@ -1,6 +1,6 @@
 import * as THREE from "three";
 import * as dat from "lil-gui";
-
+import gsap from "gsap";
 /**
  * Debug
  */
@@ -141,8 +141,21 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // scroll
 let scrollY = window.scrollY;
+let currentSection = 0;
+
 window.addEventListener("scroll", () => {
 	scrollY = window.scrollY;
+	const newSection = Math.round(scrollY / sizes.height);
+
+	if (newSection != currentSection) {
+		currentSection = newSection;
+		gsap.to(sectionMeshes[currentSection].rotation, {
+			duration: 1.5,
+			ease: "power2.inOut",
+			x: "+=6",
+			y: "+=3",
+		});
+	}
 });
 // curosr
 const cursor = {};
@@ -174,8 +187,8 @@ const tick = () => {
 		parallaxY - cameraGroup.position.y * 5 * deltaTime;
 	// animate meshes
 	for (const mesh of sectionMeshes) {
-		mesh.rotation.x = elapsedTime * 0.1;
-		mesh.rotation.y = elapsedTime * 0.1;
+		mesh.rotation.x += deltaTime * 0.1;
+		mesh.rotation.y += deltaTime * 0.12;
 	}
 
 	// Render
